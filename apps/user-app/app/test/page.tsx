2fb90a4148a27c2ca45eaaf032a87106b36d1943
Lokesh Saini <lokeshsainipilani@gmail.com>
2025-04-30 18:25:22 +0530

    removed file

--- apps/user-app/app/test/page.tsx
+++ apps/user-app/app/test/page.tsx
@@ -10,3 +10,16 @@ const page = () => {
 }
 
 export default page
+
+
+// "use client"
+
+// import {useBalance} from "@repo/store/useBalance"
+
+// export default function(){
+//     const balance = useBalance();
+//     return <div>
+//         hi there
+//         {balance}
+//     </div>
+// }
