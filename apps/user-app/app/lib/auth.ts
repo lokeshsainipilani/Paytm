82cc4b69eb23b7f0c4046dc267508d58aa347a42
Lokesh Saini <lokeshsainipilani@gmail.com>
2025-04-30 19:24:37 +0530

    auth

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,67 +1,162 @@
-import {PrismaClient} from "@repo/db/client"
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from 'bcrypt'
-import { pages } from "next/dist/build/templates/app-page";
+// import {PrismaClient} from "@repo/db/client"
+// import CredentialsProvider from "next-auth/providers/credentials"
+// import bcrypt from 'bcrypt'
 
 
-const db = new PrismaClient();
 
-export const authOptions = {
-    providers:[
-        CredentialsProvider({
-            name:'Credentials',
-            credentials:{
-                phone:{label:"phone number", type:"text", placeholder:"123123123"},
-                password:{label:"password", type:"password", placeholder:"password"}
-            },
-            async authorize(credentials:any){
-                const hashedPassword= await bcrypt.hash(credentials.password,10)
-                const existingUser = await db.user.findFirst({
-                    where:{
-                        number:credentials.phone
-                    }
-                })
-                if(existingUser){
-                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
-                    if(passwordValidation){
-                        return {
-                            id:existingUser.id.toString(),
-                            name:existingUser.name,
-                            email:existingUser.number
-                        }
-                    }
-                    return null;
-                }
-                try{
-                    const user = await db.user.create({
-                        data:{
-                            number:credentials.phone,
-                            password:hashedPassword
-                        }
-                    })
-                    return {
-                        id:user.id.toString(),
-                        name:user.name,
-                        email:user.number
-                    }
-                }
-                catch(e){
-                    console.error(e)
-                }
-                return null;
-            }
-        })
-    ],
-    secret:process.env.JWT_SECRET || "secret",
-    callbacks:{
-        async session({token, session}:any){
-            session.user.id = token.sub
+// const db = new PrismaClient();
 
-            return session
-        }
-    },
-    pages:{
-        signIn:"/auth"
-    }
+// export const authOptions = {
+//     providers:[
+//         CredentialsProvider({
+//             name:'Credentials',
+//             credentials:{
+//                 phone:{label:"phone number", type:"text", placeholder:"123123123"},
+//                 password:{label:"password", type:"password", placeholder:"password"}
+//             },
+//             async authorize(credentials:any){
+//                 const hashedPassword= await bcrypt.hash(credentials.password,10)
+//                 const existingUser = await db.user.findFirst({
+//                     where:{
+//                         number:credentials.phone
+//                     }
+//                 })
+//                 if(existingUser){
+//                     const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+//                     if(passwordValidation){
+//                         return {
+//                             id:existingUser.id.toString(),
+//                             name:existingUser.name,
+//                             email:existingUser.number
+//                         }
+//                     }
+//                     return null;
+//                 }
+//                 try{
+//                     const user = await db.user.create({
+//                         data:{
+//                             number:credentials.phone,
+//                             password:hashedPassword
+//                         }
+//                     })
+//                     return {
+//                         id:user.id.toString(),
+//                         name:user.name,
+//                         email:user.number
+//                     }
+//                 }
+//                 catch(e){
+//                     console.error(e)
+//                 }
+//                 return null;
+//             }
+//         })
+//     ],
+//     secret:process.env.JWT_SECRET || "secret",
+//     callbacks:{
+//         async session({token, session}:any){
+//             session.user.id = token.sub
+
+//             return session
+//         }
+//     },
+//     pages:{
+//         signIn:"/auth"
+//     }
     
-}
+// }
+
+
+
+import {PrismaClient} from "@repo/db/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+
+const db = new PrismaClient()
+const credentialsSchema = z.object({
+  phone: z.string().min(6).max(12),
+  password: z.string(),
+});
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Phone",
+      credentials: {
+        phone: {
+          label: "Phone number",
+          type: "text",
+          placeholder: "1231231231",
+          required: true,
+        },
+        password: { label: "Password", type: "password", required: true },
+      },
+
+      async authorize(credentials?: { phone: string; password: string }) {
+        if (!credentials || !credentials.phone || !credentials.password) {
+          console.error("Missing credentials");
+          return null;
+        }
+   
+        const result = credentialsSchema.safeParse(credentials);
+        if (!result.success) {
+          console.error(result.error);
+          return null;
+        }
+
+        const hashedPassword = await bcrypt.hash(credentials.password, 10);
+        const existingUser = await db.user.findFirst({
+          where: {
+            number: credentials.phone,
+          },
+        });
+
+        if (existingUser) {
+          const passwordValidation = await bcrypt.compare(
+            credentials.password,
+            existingUser.password,
+          );
+          if (passwordValidation) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              email: existingUser.number,
+            };
+          }
+          return null;
+        }
+
+        try {
+          const user = await db.user.create({
+            data: {
+              number: credentials.phone,
+              password: hashedPassword,
+            },
+          });
+
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.number,
+          };
+        } catch (e) {
+          console.error(e);
+        }
+
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.JWT_SECRET || "secret",
+  callbacks: {
+    // TODO: can u fix the type here? Using any is bad
+    async session({ session, token }: any) {
+      // Assuming `sub` is the user ID stored in the token
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth",
+  },
+};
